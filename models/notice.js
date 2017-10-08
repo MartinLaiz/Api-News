abd8d24cb654f556b1bc4fd9ad385e28ecdb722c
MartinLaiz <martinlaizg@gmail.com>
2017-10-08 19:17:55 +0200

    Simply version of models

---
+++ models/notice.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+var noticeSchema = new mongoose.Schema({
+    title : String,
+    description : String,
+    author : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    category : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Category'
+    },
+    publishdate : {
+        type : Date,
+        default : Date.now
+    },
+    views : Number,
+    keywords : [{
+        keyword : String,
+
+    }],
+    comments : [{
+        comment : String,
+        user : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : 'User'
+        },
+        date : Date
+    }]
+})
+
+module.exports = mongoose.model('Notice', noticeSchema)
