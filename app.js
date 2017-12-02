a9d42c52e484e18406387ca5b61f2b7423f82554
Martin Laiz <martinlaizg@gmail.com>
2017-12-02 21:01:50 +0100

    Input update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -7,23 +7,22 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-// Base de datos local = mongodb://localhost:27017
-// Base de datos externa = mongodb://apiadi:apiadi@ds113925.mlab.com:13925/newsapp
 mongoose.connect('mongodb://apiadi:apiadi@ds113925.mlab.com:13925/newsapp',{useMongoClient : true}, function(err){
     if(err) {
-        console.log('no connect mlab database');
-        mongoose.connect('mongodb://localhost:27017', {useMongoClient : true}, function(err){
+		console.log('no connect mlab database');
+		mongoose.connect('mongodb://localhost:27017', {useMongoClient : true}, function(err){
             if(err) {
                 console.log('no connect localhost database')
-            }
-            console.log('Connected to localhost database');
+			}
+			else {
+				console.log('Connected to localhost database');				
+			}
         })
     }
     else {
         console.log('Connected to mlab database')
-    }
+	}
 })
 
 app.use('/',routes)
